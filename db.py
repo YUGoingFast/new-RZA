9370bbd52b891f99eacd5a12ca1ef3d4780b29e1
Vicente <vicent.vasquez.perez@gmail.com>
2025-01-31 11:41:45 +0000

    Init

---
+++ db.py
@@ -0,0 +1,19 @@
+import sqlite3
+from flask import g, Flask, render_template, request, redirect, url_for, session
+app = Flask(__name__)
+
+if __name__ =="__main__":
+    app.run(debug=True)
+
+conn = sqlite3.connect('RZA.db', check_same_thread=False)
+cursor = conn.cursor()
+
+def add_user(fname, lname, email, password):
+    cursor.execute("INSERT INTO users (FirstName, LastName, Email, Password) VALUES (?, ?, ?, ?)", (fname, lname, email, password))
+    conn.commit()
+    conn.close()
+
+def verify_user(email, password):
+    cursor.execute("SELECT * FROM users WHERE Email = ? AND password = ?", (email, password))
+    user = cursor.fetchone()
+    return user
---
+++ users.py
@@ -0,0 +1,7 @@
+class user:
+    def __init__(self, id, fname, sname, email, password):
+        self.id = id
+        self.fname = fname
+        self.sname = sname
+        self.email = email
+        self.password = password
